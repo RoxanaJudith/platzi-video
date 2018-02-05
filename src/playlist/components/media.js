0b3c74acc5eb07e95969102a66c8c839926108bb
RoxanaJudith <roxanajpr.4@gmail.com>
2018-02-05 18:06:58 -0400

    Estilos en ReactJS

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,21 +1,32 @@
 import React from 'react';
 //import React, {Component} from 'react';
+import './media.css';
 
 
 class Media extends React.Component{
 	render(){
+		const styles ={
+			container:{
+				
+				color:'#44546b',
+				curso:'pointer',
+				width:260,
+				border:'1px solid red'
+			}
+		}
 		return(
-			 <div>
+			 //<div style={styles.container}>
+			 <div className="Media">
 			 <div>
 			 	<img 
-			 	  src=""
+			 	  src="./images/covers/bitcoin.jpg"
 			 	  alt=""
 			 	  width={260}
 			 	  height ={160}
 			 	  />
 			 	  <h3>Porque aprender React?</h3>
 			 	  <p>RoxanaJudith</p>
-			 </div>
+			 </div>			 
 			 </div>
 
 			)
